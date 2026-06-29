bd3217c8d8e0fd5845c62b5a016ce1de121548cb
kshku <1211shree@gmail.com>
2026-06-29 20:30:40 +0530

    feat: run tests

--- test/src/sntest.c
+++ test/src/sntest.c
@@ -1,7 +1,25 @@
 #include "sntest/sntest.h"
 
-#include <stdio.h>
+int sn_test_run_all_tests(SnTestConfig *config) {
+    SN_UNUSED(config);
+#if defined(SN_OS_MAC)
+    size_t count = 0;
+    SnTest **tests = SN_TEST_BEGIN_COUNT(count);
 
-void test_print(void) {
-    printf("Hello, World!\n");
+    for (size_t i = 0; i < count; ++i) {
+        SnTest *it = tests[i];
+#else
+    SnTest **begin = SN_TEST_BEGIN();
+    SnTest **end = SN_TEST_END();
+
+    for (SnTest **p = begin; p < end; ++p) {
+        SnTest *it = *p;
+#endif
+        if (!it) continue;
+
+        // log_info("Running test: %s", it->name);
+        it->fn();
+    }
+
+    return 0;
 }
--- tests/test.c
+++ tests/test.c
@@ -1,5 +1,17 @@
 #include <sntest/sntest.h>
+#include <stdio.h>
 
-int main(void) {
-    test_print();
+bool sn_test_init(SnTestConfig *config) {
+    SN_UNUSED(config);
+    return true;
 }
+
+void sn_test_deinit(void) {
+}
+
+SN_TEST_ADD(print_test) {
+    printf("inside test!\n");
+    return SN_TEST_PASS;
+}
+
+SN_TEST_RUN()
